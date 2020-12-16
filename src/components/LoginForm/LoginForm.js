75fd9b1f1cba35042fa71f87c1948c6bfb6ee618
Timothy Hanke <tim.hanke@gmail.com>
2020-12-15 20:08:56 -0500

    revisions for protected endpoints lesson

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,60 +1,49 @@
-import React, { Component } from 'react'
-import { Button, Input } from '../Utils/Utils'
+import React, { Component } from "react";
+import TokenService from "../../services/token-service";
+import { Button, Input } from "../Utils/Utils";
 
 export default class LoginForm extends Component {
   static defaultProps = {
-    onLoginSuccess: () => {}
-  }
+    onLoginSuccess: () => {},
+  };
 
-  state = { error: null }
+  state = { error: null };
 
-  handleSubmitBasicAuth = ev => {
-    ev.preventDefault()
-    const { user_name, password } = ev.target
+  handleSubmitBasicAuth = (ev) => {
+    ev.preventDefault();
+    const { user_name, password } = ev.target;
 
-    console.log('login form submitted')
-    console.log({ user_name, password })
+    console.log("login form submitted");
+    console.log({ user_name, password });
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name.value, password.value)
+    );
 
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
-  }
+    user_name.value = "";
+    password.value = "";
+    this.props.onLoginSuccess();
+  };
 
   render() {
-    const { error } = this.state
+    const { error } = this.state;
     return (
-      <form
-        className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
-      >
-        <div role='alert'>
-          {error && <p className='red'>{error}</p>}
+      <form className="LoginForm" onSubmit={this.handleSubmitBasicAuth}>
+        <div role="alert">{error && <p className="red">{error}</p>}</div>
+        <div className="user_name">
+          <label htmlFor="LoginForm__user_name">User name</label>
+          <Input required name="user_name" id="LoginForm__user_name"></Input>
         </div>
-        <div className='user_name'>
-          <label htmlFor='LoginForm__user_name'>
-            User name
-          </label>
+        <div className="password">
+          <label htmlFor="LoginForm__password">Password</label>
           <Input
             required
-            name='user_name'
-            id='LoginForm__user_name'>
-          </Input>
+            name="password"
+            type="password"
+            id="LoginForm__password"
+          ></Input>
         </div>
-        <div className='password'>
-          <label htmlFor='LoginForm__password'>
-            Password
-          </label>
-          <Input
-            required
-            name='password'
-            type='password'
-            id='LoginForm__password'>
-          </Input>
-        </div>
-        <Button type='submit'>
-          Login
-        </Button>
+        <Button type="submit">Login</Button>
       </form>
-    )
+    );
   }
 }
--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,57 +1,48 @@
-import config from '../config'
+import TokenService from "./token-service";
+import config from "../config";
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
-      headers: {
-      },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+      headers: {},
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
   },
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
+        Authorization: `basic ${TokenService.getAuthToken()}`,
       },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
   },
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        Authorization: `basic ${TokenService.getAuthToken()}`,
       },
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
   },
   postReview(thingId, text, rating) {
     return fetch(`${config.API_ENDPOINT}/reviews`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
+        Authorization: `basic ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         thing_id: thingId,
         rating,
         text,
       }),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  }
-}
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
 
-export default ThingApiService
+export default ThingApiService;
