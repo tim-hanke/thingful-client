0d5e9156fefc5c693962d1ed0ce6c8e0ab94b8a0
Timothy Hanke <tim.hanke@gmail.com>
2020-12-20 05:53:25 -0500

    modified to use jwt auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TokenService from "../../services/token-service";
+import AuthApiService from "../../services/auth-api-service";
 import { Button, Input } from "../Utils/Utils";
 
 export default class LoginForm extends Component {
@@ -9,25 +10,30 @@ export default class LoginForm extends Component {
 
   state = { error: null };
 
-  handleSubmitBasicAuth = (ev) => {
+  handleSubmitJwtAuth = (ev) => {
     ev.preventDefault();
+    this.setState({ error: null });
     const { user_name, password } = ev.target;
 
-    console.log("login form submitted");
-    console.log({ user_name, password });
-    TokenService.saveAuthToken(
-      TokenService.makeBasicAuthToken(user_name.value, password.value)
-    );
-
-    user_name.value = "";
-    password.value = "";
-    this.props.onLoginSuccess();
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then((res) => {
+        user_name.value = "";
+        password.value = "";
+        TokenService.saveAuthToken(res.authToken);
+        this.props.onLoginSuccess();
+      })
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
   };
 
   render() {
     const { error } = this.state;
     return (
-      <form className="LoginForm" onSubmit={this.handleSubmitBasicAuth}>
+      <form className="LoginForm" onSubmit={this.handleSubmitJwtAuth}>
         <div role="alert">{error && <p className="red">{error}</p>}</div>
         <div className="user_name">
           <label htmlFor="LoginForm__user_name">User name</label>
--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -12,7 +12,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
-        Authorization: `basic ${TokenService.getAuthToken()}`,
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
@@ -21,7 +21,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
-        Authorization: `basic ${TokenService.getAuthToken()}`,
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
       },
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
@@ -32,7 +32,7 @@ const ThingApiService = {
       method: "POST",
       headers: {
         "content-type": "application/json",
-        Authorization: `basic ${TokenService.getAuthToken()}`,
+        Authorization: `bearer ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         thing_id: thingId,
